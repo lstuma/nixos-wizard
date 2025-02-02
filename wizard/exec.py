76f90b6dd97bce9a0357cbd676cff7f445e1c83b
lstuma <lstuma@lstuma.dev>
2025-02-02 23:10:20 +0100

    fixed code for commands

--- wizard/exec.py
+++ wizard/exec.py
@@ -2,33 +2,36 @@ import subprocess
 from .settings import get_option
 from .log import exception
 
-def execute(bash_code: str, capture: bool = False, allow_single_quotes: bool = False) -> str | None:
+def execute(command: str, capture: bool = False, allow_single_quotes: bool = False) -> str | None:
     """
-    Execute the given code using bash.
+    Execute the given code.
 
-    :param bash_code: The code to execute.
+    :param code: The code to execute.
     :param capture: Whether to capture the output or simply print it.
     :return: The output of the code if capture is True.
     """
-    if "'" in bash_code and not allow_single_quotes:
-        raise ValueError("Single quotes are not allowed in bash code.")
-    bash_binary = get_option("bash")
-    command = f"{bash_binary} -c '{bash_code}'"
     if not capture:
-        subprocess.run(command, shell=True)
+        try:
+            subprocess.run(command, shell=True).check_returncode()
+        except subprocess.CalledProcessError as e:
+            exception(f"Error executing command `{command}`", e)
+            raise e
         return
 
     with subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+        proc.wait()
         stdout, stderr = proc.communicate()
         error_output = stderr.decode()
-        if error_output:
+        return_code = proc.returncode
+        if return_code != 0:
             exception("Error executing command", RuntimeError(error_output))
             raise RuntimeError(f"Error executing command `{command}`: {error_output}")
         return stdout.decode()
 
-def nix_exec(bash_code: str, capture: bool = False, packages: list[str] | None = None) -> str:
-    if "'" in bash_code:
-        raise ValueError("Single quotes are not allowed in bash code.")
+def nix_exec(command: str, capture: bool = False, packages: list[str] | None = None) -> str:
+    if '"' in command:
+        raise ValueError("Double quotes are not allowed in nix_exec")
     packages = packages or []
+    packages += ["bash"]
     packages_str = " ".join(packages)
-    return execute(f"nix-shell -p \\'{packages_str}\\' --run \\'{bash_code}\\'", capture=capture, allow_single_quotes=True) or ''
+    return execute(f"nix-shell --packages {packages_str} --quiet --run \"{command}\"", capture=capture) or ''
